b2df382888a9286bcc52262371bf064f692853b1
ScrabbleScratch <55005341+ScrabbleScratch@users.noreply.github.com>
2020-12-13 02:04:21 -0600

    Revert "Lectura de csv por medio de strings"

--- SQLTable/SQLTable.py
+++ SQLTable/SQLTable.py
@@ -6,36 +6,6 @@ import errno
 from os import strerror
 from re import search
 
-# Simple parsing function 
-# TODO: correctly unescape characters 
-
-def parse(line):
-    tableCols = []
-    cur = 0
-    length = len(line)
-    while cur < length:
-        comma = line.find(",", cur)
-        quote = line.find('"', cur)
-        if comma == -1 and quote == -1:
-            tableCols.append(line[cur:].strip())
-            cur = length + 1
-        elif comma < quote or quote == -1:
-            tableCols.append(line[cur:comma])
-            cur = comma + 1
-        else:
-            quote = line.find('"', cur + 1)
-            double = line.find('""', cur + 1)
-            temp = cur
-            while double != -1 and quote == double:
-                double += 2
-                quote = line.find('"', double)
-                double = line.find('""', double)
-                if double != -1:
-                    temp = double
-            tableCols.append(line[cur + 1:line.find('"', temp + 2)].replace('""', '"'))
-            cur = line.find(",", line.find('"', temp + 2)) + 1
-    return tableCols
-
 # open the table to be converted
 while True:
     try:
